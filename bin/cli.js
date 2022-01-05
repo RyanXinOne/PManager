27208dc29d30e639964405456b5c03ba011c6a6f
RyanXin <xyz@ryanxin.cn>
2022-01-05 00:17:54 +0000

    dev: import minimist

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,36 @@
 #!/usr/bin/env node
 const storage = require('./storage.js');
+const parseArgs = require('minimist');
 
 function print(obj) {
-    if (obj) {
+    if (typeof obj === 'object') {
         console.log(JSON.stringify(obj, null, 2));
+    } else {
+        console.log(obj);
     }
 }
 
-// test code
-let res = storage.get([]);
-print(res);
-storage.set(['2', '2-1'], 'v2-21');
-storage.set(['2', '2-3'], 'v2-3');
-storage.set(['3'], 'v3');
+const argOpts = {
+    string: '_',
+    boolean: ['h', 'e', 'd', 'f'],
+    alias: {'h': 'help', 'e': ['m', 'edit'], 'd': 'delete', 'f': 'force'},
+    default: {},
+    stopEarly: false
+};
+let args = parseArgs(process.argv.slice(2), argOpts);
 
-storage.delete(["3"], force = true);
+if (args.help) {
+    print(`
+    Usage:
+        pm <key> [<value>]
+        pm <key> --edit
+        pm <key> --delete
+        pm --help
+    `);
+} else if (args.edit) {
+    // set
+} else if (args.delete) {
+    // delete
+} else {
+    // get
+}
--- bin/storage.js
+++ bin/storage.js
@@ -1,13 +1,25 @@
-const fs = require('fs');
 const config = require('./config.js');
+const fs = require('fs');
+
+
+function response(success, message = null, data = null) {
+    return {
+        success: success,
+        message: message,
+        data: data
+    };
+}
 
 /**
  * Get a value from filestore by key chain specified.
+ * Return string of given key chain in filestore.
  * 
  * @param {String | Array.<string>} keyChain
- * @returns {String | null} value of given key chain in filestore
  */
 function _get(keyChain) {
+    if (keyChain.length === 0) {
+        return response(false, 'Key chain is empty.');
+    }
     if (!Array.isArray(keyChain)) {
         keyChain = [keyChain];
     }
@@ -16,20 +28,18 @@ function _get(keyChain) {
         data = JSON.parse(data);
         let obj = data;
         let i;
-        for (i = 0; i < keyChain.length - 1; i++) {
+        for (i = 0; i < keyChain.length; i++) {
             // iterate into next key
             obj = obj[keyChain[i]];
             // check undefined
             if (!obj) {
-                console.warn('Key "' + keyChain.slice(0, i + 1).join('::') + '" does not exist.');
-                return null;
+                return response(false, 'Key "' + keyChain.slice(0, i + 1).join('::') + '" does not exist.');
             }
         }
         // get value from the last key
-        return obj[keyChain[i]];
+        return response(true, null, obj);
     } catch (err) {
-        console.error(err.message);
-        return null;
+        return response(false, err.message);
     }
 }
 
@@ -39,10 +49,12 @@ function _get(keyChain) {
  * @param {String | Array.<string>} keyChain
  * @param {String} value
  * @param {Boolean} createObject create new object if any non-final key does not exist
- * @param {Boolean} OverwriteObject force to overwrite even if key chain points to an existing object
- * @returns {Boolean} true if success, false otherwise
+ * @param {Boolean} force force to overwrite even if key chain points to an existing object
  */
-function _set(keyChain, value, createObject = false, OverwriteObject = false) {
+function _set(keyChain, value, createObject = false, force = false) {
+    if (keyChain.length === 0) {
+        return response(false, 'Key chain is empty.');
+    }
     if (!Array.isArray(keyChain)) {
         keyChain = [keyChain];
     }
@@ -57,24 +69,22 @@ function _set(keyChain, value, createObject = false, OverwriteObject = false) {
                 if (createObject) {
                     obj[keyChain[i]] = {};
                 } else {
-                    console.warn('Key "' + keyChain.slice(0, i + 1).join('::') + '" does not exist.');
-                    return false;
+                    return response(false, 'Key "' + keyChain.slice(0, i + 1).join('::') + '" does not exist.');
                 }
             }
             // iterate into next key
             obj = obj[keyChain[i]];
         }
-        // update value to the last key
-        if (typeof obj[keyChain[i]] === 'object' && !OverwriteObject) {
-            console.warn('Key "' + keyChain.join('::') + '" is not a plain value. Use `OverwriteObject` to overwrite.');
-            return false;
+        // check object
+        if (typeof obj[keyChain[i]] === 'object' && !force) {
+            return response(false, 'Key "' + keyChain.join('::') + '" is not a plain value. Use `force` to overwrite.');
         }
+        // update value to the final key
         obj[keyChain[i]] = value;
         fs.writeFileSync(config.fileStore, JSON.stringify(data, null, 4));
-        return true;
+        return response(true);
     } catch (err) {
-        console.error(err.message);
-        return false;
+        return response(false, err.message);
     }
 }
 
@@ -83,9 +93,11 @@ function _set(keyChain, value, createObject = false, OverwriteObject = false) {
  * 
  * @param {String | Array.<string>} keyChain
  * @param {Boolean} force force to delete even if key chain points to an object
- * @returns true if success, false otherwise
  */
 function _delete(keyChain, force = false) {
+    if (keyChain.length === 0) {
+        return response(false, 'Key chain is empty.');
+    }
     if (!Array.isArray(keyChain)) {
         keyChain = [keyChain];
     }
@@ -99,26 +111,23 @@ function _delete(keyChain, force = false) {
             obj = obj[keyChain[i]];
             // check existence
             if (!obj) {
-                console.warn('Key "' + keyChain.slice(0, i + 1).join('::') + '" does not exist.');
-                return false;
+                return response(false, 'Key "' + keyChain.slice(0, i + 1).join('::') + '" does not exist.');
             }
         }
-        // delete value from the last key
+        // check existence
+        if (!(keyChain[i] in obj)) {
+            return response(false, 'Key "' + keyChain.join('::') + '" does not exist.');
+        }
+        // check object
         if (typeof obj[keyChain[i]] === 'object' && !force) {
-            console.warn('Key "' + keyChain.join('::') + '" is not a plain value. Use `force` to delete.');
-            return false;
+            return response(false, 'Key "' + keyChain.join('::') + '" is not a plain value. Use `force` to delete.');
         }
-        if (keyChain[i] in obj) {
-            delete obj[keyChain[i]];
-        } else {
-            console.warn('Key "' + keyChain.join('::') + '" does not exist.');
-            return false;
-        }
+        // delete value from the last key
+        delete obj[keyChain[i]];
         fs.writeFileSync(config.fileStore, JSON.stringify(data, null, 4));
-        return true;
+        return response(true);
     } catch (err) {
-        console.error(err.message);
-        return false;
+        return response(false, err.message);
     }
 }
 
