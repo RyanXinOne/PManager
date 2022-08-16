dd92d81c13760637c28cf0a0e7b0c23bf1605264
RyanXin <xyz@ryanxin.cn>
2022-08-16 22:34:23 +0100

    Read config from file

--- bin/config.js
+++ bin/config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 
 const sysAppDataFolder = process.env.APPDATA ||
@@ -7,12 +8,36 @@ const sysAppDataFolder = process.env.APPDATA ||
         path.join(process.env.HOME, '.local', 'share')
     );
 const pmDataFolder = process.env.PMENV === 'dev' ?
-    path.join(__dirname, '..') :
+    path.join(__dirname, '..', 'pmdata') :
     path.join(sysAppDataFolder, 'pmanager');
 
-const config = {
+const defaultConfig = {
     fileStoragePath: path.join(pmDataFolder, 'PMDATA'),
     passphrase: 'AVERYSTRONGPASSWORD'
 };
+
+// initialise config file if non-existent
+const configPath = path.join(pmDataFolder, 'config.json');
+if (!fs.existsSync(configPath)) {
+    fs.mkdirSync(path.dirname(configPath), { recursive: true });
+    try {
+        let configText = JSON.stringify({}, null, 4);
+        fs.writeFileSync(configPath, configText, 'utf8');
+    } catch (err) {
+        console.error("Failed to initialise config file: %s", res.message);
+        process.exit(0);
+    }
+}
+
+// read config file
+let config;
+try {
+    let configText = fs.readFileSync(configPath, 'utf8');
+    let fileConfig = JSON.parse(configText);
+    config = { ...defaultConfig, ...fileConfig };
+} catch (err) {
+    console.error("Failed to read or parse config file: %s", err.message);
+    process.exit(0);
+}
 
 module.exports = config;
