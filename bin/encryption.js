90308f6b1b676ccd43cee803b6e0b2a97ed52c08
RyanXin <xyz@ryanxin.cn>
2022-08-20 20:27:40 +0100

    Memorise active key

--- bin/encryption.js
+++ bin/encryption.js
@@ -11,48 +11,77 @@ const ALGORITHM_KEY_SIZE = 32;
 const ALGORITHM_NONCE_SIZE = 16;
 const ALGORITHM_TAG_SIZE = 16;
 
-function _flushKeyCache(path, salt) {
+let ACTIVE_KEY = undefined;
+
+/**
+ * Flush key cache by asking user passphrase from command line input. The key is
+ * generated from passphrase and saved into local cache file.
+ * 
+ * @param {string} path key cache file path
+ */
+function _flushKeyCache(path) {
     const readlineOptions = { hideEchoBack: true, mask: '', keepWhitespace: true, caseSensitive: true };
     let passphrase = readlineSync.question('Passphrase (not displayed): ', readlineOptions);
+    let salt = crypto.createHash('sha256').update(passphrase).digest();
     let key = crypto.scryptSync(passphrase, salt, ALGORITHM_KEY_SIZE);
     fs.writeFileSync(path, key);
 }
 
-function _getKey(salt) {
-    const keyCachePath = path.join(os.tmpdir(), 'pmanager', 'KEYCACHE');
-
-    if (!fs.existsSync(keyCachePath)) {
+/**
+ * Get the key that is used for encryption.
+ * This method uses existing active key to avoid fetch from cache multiple times in one process.
+ * If no active key, then try to fetch from cache.
+ * If failed to fetch from cache, then flush key cache.
+ * 
+ * @returns {Buffer} encryption key
+ */
+function _getKey() {
+    if (ACTIVE_KEY === undefined) {
+        const keyCachePath = path.join(os.tmpdir(), 'pmanager', 'KEYCACHE');
         // check if key cache exists
-        fs.mkdirSync(path.dirname(keyCachePath), { recursive: true });
-        _flushKeyCache(keyCachePath, salt);
-    } else {
-        // check if key cache expires
-        let cacheStat = fs.statSync(keyCachePath);
-        let expireTime = Number(config.doNotAskPassphraseInSec) || 0;
-        if (Date.now() - cacheStat.mtime.getTime() > 1000 * expireTime) {
-            _flushKeyCache(keyCachePath, salt);
+        if (!fs.existsSync(keyCachePath)) {
+            fs.mkdirSync(path.dirname(keyCachePath), { recursive: true });
+            _flushKeyCache(keyCachePath);
+        } else {
+            // check if key cache expires
+            let cacheStat = fs.statSync(keyCachePath);
+            let expireTime = Number(config.doNotAskPassphraseInSec) || 0;
+            if (Date.now() - cacheStat.mtime.getTime() > 1000 * expireTime) {
+                _flushKeyCache(keyCachePath);
+            }
         }
+        // read key from key cache
+        ACTIVE_KEY = fs.readFileSync(keyCachePath)
     }
-    // read key from key cache
-    return fs.readFileSync(keyCachePath);
+    return ACTIVE_KEY;
 }
 
+/**
+ * Encrypt a text string.
+ * 
+ * @param {string} dataStr text string to encrypt
+ * @returns {Buffer} encrypted binary buffer
+ */
 function _encrypt(dataStr) {
     let nonce = crypto.randomBytes(ALGORITHM_NONCE_SIZE);
-    let key = _getKey(nonce);
-    let cipher = crypto.createCipheriv(ALGORITHM_NAME, key, nonce, { authTagLength: ALGORITHM_TAG_SIZE });
+    let cipher = crypto.createCipheriv(ALGORITHM_NAME, _getKey(), nonce, { authTagLength: ALGORITHM_TAG_SIZE });
     let cipherText = Buffer.concat([cipher.update(dataStr, 'utf8'), cipher.final()]);
 
     return Buffer.concat([nonce, cipherText, cipher.getAuthTag()]);
 }
 
+/**
+ * Decrypt a binary buffer.
+ * 
+ * @param {Buffer} dataBuf binary buffer to decrypt
+ * @returns {string} decrypted text string
+ */
 function _decrypt(dataBuf) {
-    let nonce = dataBuf.slice(0, ALGORITHM_NONCE_SIZE);
-    let key = _getKey(nonce);
-    let decipher = crypto.createDecipheriv(ALGORITHM_NAME, key, nonce, { authTagLength: ALGORITHM_TAG_SIZE });
-    let cipherText = dataBuf.slice(ALGORITHM_NONCE_SIZE, dataBuf.length - ALGORITHM_TAG_SIZE);
+    let nonce = dataBuf.subarray(0, ALGORITHM_NONCE_SIZE);
+    let decipher = crypto.createDecipheriv(ALGORITHM_NAME, _getKey(), nonce, { authTagLength: ALGORITHM_TAG_SIZE });
+    let cipherText = dataBuf.subarray(ALGORITHM_NONCE_SIZE, dataBuf.length - ALGORITHM_TAG_SIZE);
 
-    let authTag = dataBuf.slice(cipherText.length + ALGORITHM_NONCE_SIZE);
+    let authTag = dataBuf.subarray(cipherText.length + ALGORITHM_NONCE_SIZE);
     decipher.setAuthTag(authTag);
 
     return decipher.update(cipherText, null, 'utf8') + decipher.final('utf8');
