016be576f5106c1acfa0a062133d915b7b77785f
RyanXin <xyz@ryanxin.cn>
2022-08-22 19:46:16 +0100

    Fix key cache logic

--- bin/encryption.js
+++ bin/encryption.js
@@ -15,18 +15,17 @@ const PASSPHRASE_TRIAL_LIMIT = 5;
 const KEY_CACHE_PATH = path.join(os.tmpdir(), 'pmanager', 'KEYCACHE');
 fs.mkdirSync(path.dirname(KEY_CACHE_PATH), { recursive: true });
 
-let ACTIVE_KEY;
-_setActiveKey('');  // try empty passphrase by default
+let ACTIVE_KEY = undefined;
 
 /**
  * Flush local key cache by input passphrase. If not provided, ask user passphrase
  * from command line input. The key is generated from passphrase and saved into
  * local temporary cache file.
  * 
- * @param {string} passphrase passphrase to generate key from
+ * @param {string} passphrase passphrase to generate key from. If null, ask from command line input.
  */
-function _flushKeyCache(passphrase = undefined) {
-    if (passphrase === undefined) {
+function _flushKeyCache(passphrase) {
+    if (passphrase === null) {
         passphrase = askSecret('Passphrase (not displayed): ');
     }
     let salt = crypto.createHash('sha256').update(passphrase).digest();
@@ -43,24 +42,30 @@ function _flushKeyCache(passphrase = undefined) {
  * Set up ACTIVE_KEY that is used for encryption.
  * If already set up, use existing active key to avoid fetching multiple times from cache.
  * If no active key, then fetch key from cache.
- * If cache is not ready, then flush key cache.
+ * If cache cannot be trusted, then flush key cache. (whenever flush, try empty passphrase before having to ask user)
+ * 
+ * @param {boolean} forceFlush force to flush key cache
  */
-function _prepareActiveKey() {
-    // check if active key is already set up
+function _prepareActiveKey(forceFlush = false) {
+    // check if ACTIVE_KEY is already set up
     if (ACTIVE_KEY !== undefined) {
         return;
     }
-    // check if key cache exists
-    if (!fs.existsSync(KEY_CACHE_PATH)) {
-        _flushKeyCache();
+    // prepare key cache
+    if (forceFlush) {
+        _flushKeyCache(null);
+    } else if (!fs.existsSync(KEY_CACHE_PATH)) {
+        // check key cache existence
+        _flushKeyCache('');
     } else {
         // check if key cache expires
         let cacheStat = fs.statSync(KEY_CACHE_PATH);
         let expireTime = Number(config.doNotAskPassphraseInSec) || 0;
         if (Date.now() - cacheStat.mtime.getTime() > 1000 * expireTime) {
-            _flushKeyCache();
+            _flushKeyCache('');
         }
     }
+    // now key cache is ready and trusted, read ACTIVE_KEY from cache
     try {
         ACTIVE_KEY = fs.readFileSync(KEY_CACHE_PATH);
     } catch (err) {
@@ -111,7 +116,7 @@ function _decrypt(dataBuf, trial = 0) {
         throw new Error('Maximum passphrase trial reached.');
     }
 
-    _prepareActiveKey();
+    _prepareActiveKey(trial > 0);
     let nonce = dataBuf.subarray(0, ALGORITHM_NONCE_SIZE);
     let cipherText = dataBuf.subarray(ALGORITHM_NONCE_SIZE, dataBuf.length - ALGORITHM_TAG_SIZE);
     let authTag = dataBuf.subarray(cipherText.length + ALGORITHM_NONCE_SIZE);
